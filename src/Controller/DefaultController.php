15e834055be0a56a93598234278ad3709bb5ff58
sovietspy2 <sovietspy2@yandex.com>
2018-09-16 08:02:51 +0200

    added autowired class and figured out try catch is kinda useless in php

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -1,19 +1,34 @@
 <?php
 namespace App\Controller;
 
+use App\GreetingGenerator;
+use App\php;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+use Psr\Log\LoggerInterface;
 
 class DefaultController extends AbstractController
 {
     /**
     * @Route("/hello/{name}")
     */
-    public function index($name)
+    public function index($name, LoggerInterface $logger, GreetingGenerator $greetings) // autowiring
     {
+
+        $greet = $greetings->getRandomGreeting();
+//
+//        try {
+//            $greet =  implode(" ",$greet);
+//        } catch (Excepton $e) {
+//            $greet = "HELLO THERE KENOBI";
+//        }
+
+        $logger->info("someone is using index function with the value: $name");
         return $this->render('default/index.html.twig', [
             'name' => $name,
+            'greetings'=> $greet,
         ]);
     }
 
@@ -29,7 +44,7 @@ class DefaultController extends AbstractController
     /**
      * @Route("/api/{name}")
      */
-    public function api($name) {
+    public function apiExample($name) {
         return $this->json([
             'name' => $name,
             'wow' => 'wow',
---
+++ src/Entity/Product.php
@@ -0,0 +1,32 @@
+<?php
+// src/Entity/Product.php
+namespace App\Entity;
+
+use ApiPlatform\Core\Annotation\ApiResource;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity()
+ * @ApiResource()
+ */
+class Product
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string")
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="int")
+     */
+    private $price;
+
+// ...
+}
